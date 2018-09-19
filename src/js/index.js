6e49b5325daa74db7998dc46b37e6ba024914101
meraseudev <meraseudev@gmail.com>
2018-09-19 10:59:32 +0900

    config 수정

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,10 @@ import '../scss/index.scss'
 /**
  * @namespace Namespace
  */
+
+console.log(process.env.NODE_ENV);
+
+function importAll(r) {
+    return r.keys().map(r);
+}  
+const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
---
+++ webpack.dev.js
@@ -0,0 +1,95 @@
+var path = require('path'),
+    webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'css/[name].css'
+});
+
+var config = {
+
+    entry: {
+        'index': './src/js/index',
+        'sub': './src/js/sub'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            minimize: false
+                        }
+                    }, {
+                        loader: "sass-loader",
+                        options: {
+                            data: '@import "./src/scss/config/_config.dev";',
+                            outputStyle: 'compact' // nested, expanded, compact, compressed
+                        }
+                    }]
+                })
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                        }
+                    },
+                    'image-webpack-loader'
+                ]
+            }
+        ],
+    },
+    plugins: [
+        extractPlugin,
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/html/index.html',
+            chunks: ['index']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'sub.html',
+            template: 'src/html/sub.html',
+            chunks: ['sub']
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        })
+    ]
+};
+module.exports = config;
